2998e0e56874210f36475b927bc988631072d1cf
swalsh <stephen.walsh@alvaria.com>
2021-11-23 14:48:38 +0000

    adding tapo control

--- src/main/scala/Api/Tapo.scala
+++ src/main/scala/Api/Tapo.scala
@@ -1,23 +1,19 @@
 package Api
 
-import Api.TapoObjectMapper.{C658a, HandShakePayload, HandShakePayloadParams, HandShakeReply, KspB64, KspKeyPair, LoginPayload, LoginPayloadParams, SecurePassthroughPayload, SecurePassthroughPayloadParams}
+import Api.TapoObjectMapper._
 import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.google.gson.Gson
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.commons.codec.binary.Hex
-import org.apache.http.client.methods.{CloseableHttpResponse, HttpPost}
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.util.EntityUtils
-
 import java.util.Base64
-import java.nio.charset.StandardCharsets
 import java.security.spec.PKCS8EncodedKeySpec
-import java.security.{KeyFactory, MessageDigest, PublicKey}
-import java.text.SimpleDateFormat
-import java.util.TimeZone
+import java.security.{KeyFactory, MessageDigest}
 import javax.crypto.Cipher
 
 class Tapo {
@@ -36,7 +32,6 @@ class Tapo {
   val privateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMY+gDb9hwm9gmYB+iDYxKPngQ3XRm3U4tzea9NLtmHgWipM44tE4zWkEeaBawWbFjrnT0lPriTXPn6OvvDRCY7eRejB9wP/aezoNMMrA2XQ6Du8gcopM1ylUJCWjr9nftTb6wn75wY5IMPUKM2N0uZ+dvfQH4qEQwE05z6Iz1szAgMBAAECgYAW5UcHktZSwKlbwKSzwHVNfMJB5/gBXVHqMmH/oEHrIe8n7YNmJUmce1t55L6IgjXaDbbxf5tcM+PK2A+jXnEc8+2snDbI1PpBepEwg5vzZ8RYnhGZT6P0/PsIqInkTIZnfXDnQ6wBapO1m9xJDe/BfyWMVRa6JqJtgQ0XpfG76QJBAOgKPSF5wJKC69lCbvyh38fQoeJxEKrD03FGXGBDLLYOHqfijdMTvcnE415994MI3fTy2dWm/yB8wZ7DvYVwVX8CQQDatuZyi6LhLhU47l5vpFsOnRWGhGrZaIN0o7N/1v5Vwieujrwy/yW2uCvUeXVnmohJa+sFSr29HO4PEQwWtFxNAkBXSxrKUDJ5K9Wsa0izs/YrBrsQJDb/9yHBmJXCBSN57f/sateuE9wvXummr78AxcIyl3YJ4YRTZXu1za+r1qHjAkEAzjkavPKQ18W92PpZLOdJvFO9EiMVJH15Rad8/pNXKMFy7RJEvcj6ZHjvSt5jJxb8Xk5VQZ4hnYkDpk0qmtXhGQJBAIO6TEXkHU3Sn0qTZsPmpu+EfCADLNKG43kiv74+cRzS7Th2A6E1yq5Y/lmbdpYaHqm0mKgvMHb2ls7DtRTYoXo="
   val publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGPoA2/YcJvYJmAfog2MSj54EN10Zt1OLc3mvTS7Zh4FoqTOOLROM1pBHmgWsFmxY6509JT64k1z5+jr7w0QmO3kXowfcD/2ns6DTDKwNl0Og7vIHKKTNcpVCQlo6/Z37U2+sJ++cGOSDD1CjNjdLmfnb30B+KhEMBNOc+iM9bMwIDAQAB"
   val secretKey = "YyqsaPrgmVKcjS6UQY5aV0KtOdovqbuwGQMkZMoHtwygZQVy3fiLp0/03B4LKkYtIm0SBPYJw/cVw2DyZYsPPvX71RbuU9Pp+AJkWTfReEWhSW2vonWv6HIULK94hREHt7S51oBNXo5QP4wn2F4PD3hgP2DYBAw7guMy9wbGB+4="
-  val keyPair = new KspKeyPair(privateKey, publicKey);
   var Cookie = ""
 
   def run(): Unit = {
@@ -75,9 +70,8 @@ class Tapo {
   }
 
   def decode_handshake_key(key:String): C658a = {
-    try {
-      val decode: Array[Byte] = new KspB64().decode(secretKey.getBytes("UTF-8"));
-      val decode2: Array[Byte]= new KspB64().decode(keyPair.getPrivateKey())
+      val decode: Array[Byte] = new KspB64().decode(key.getBytes("UTF-8"));
+      val decode2: Array[Byte]= new KspB64().decode(privateKey.getBytes())
       val instance = Cipher.getInstance("RSA/ECB/PKCS1Padding");
       val kf = KeyFactory.getInstance("RSA");
       val p = kf.generatePrivate(new PKCS8EncodedKeySpec(decode2));
@@ -88,8 +82,8 @@ class Tapo {
       System.arraycopy(doFinal, 0, bArr, 0, 16);
       System.arraycopy(doFinal, 16, bArr2, 0, 16);
       new C658a(bArr, bArr2);
-    }
   }
+
   def send_login(c658a: C658a) {
     println("Calling Tapo.Send_Login()")
     val url = "http://" + addresses + "/app"
--- src/main/scala/Api/TapoObjectMapper.scala
+++ src/main/scala/Api/TapoObjectMapper.scala
@@ -57,16 +57,6 @@ object TapoObjectMapper {
     val f21777b_dec = Cipher.getInstance("AES/CBC/PKCS5Padding")
     f21777b_dec.init(Cipher.DECRYPT_MODE, key, iv)
 
-
-//    val instance = KeyPairGenerator.getInstance("RSA")
-//    instance.initialize(1024, new SecureRandom())
-//    val generateKeyPair = instance.generateKeyPair()
-//    val a: RSAPublicKey = generateKeyPair.getPublic().asInstanceOf[RSAPublicKey]
-//    val b: RSAPrivateKey = generateKeyPair.getPrivate().asInstanceOf[RSAPrivateKey]
-//    val str = new String(Base64.getEncoder.encode(a.getEncoded()))
-//    val str2 = new String(Base64.getEncoder.encode(b.getEncoded()))
-
-
     def decrypt(cipherText: String): String = {
       val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
       cipher.init(Cipher.DECRYPT_MODE, key, iv)
@@ -79,52 +69,6 @@ object TapoObjectMapper {
       cipher.init(Cipher.ENCRYPT_MODE, key, iv)
       val cipherText = cipher.doFinal(input.getBytes)
       Base64.getEncoder.encodeToString(cipherText)
-    }
-
-//    println("Text ="+testString)
-//
-//    val enc = encrypt(testString)
-//    val plainText: String = decrypt(enc)
-//
-//    println("Encrypted ="+enc)
-//    println("Decrypted ="+plainText)
-
-//    val encr = Base64.getEncoder().encodeToString(enc)
-//    val encry = encr.replace("\r\n", "")
-//
-//    println("encrypted ="+encry)
-//    val dec = f21777b_dec.doFinal(Base64.getDecoder.decode(encry))
-//    val decr = new String(dec)
-//    println("decrypted ="+decr)
-
-//    @throws[Exception]
-//    def mo38009b_enc(str: String): String = {
-//      val doFinal = f21776a_enc.doFinal(str.getBytes)
-//      val encrypted = Base64.getEncoder().encodeToString(doFinal)
-//      encrypted.replace("\r\n", "")
-//    }
-//
-//    @throws[Exception]
-//    def mo38006a_dec(str: String): String = {
-//
-//      val plainText = f21777b_dec.doFinal(Base64.getDecoder.decode(str))
-//      new String(plainText)
-
-      //val doFinal = f21777b_dec.doFinal(new KspB64().decode(str.getBytes("utf-8")))
-      //doFinal.toString
-//    }
-  }
-
-  class KspKeyPair(xprivateKey: String, xpublicKey: String) {
-    val privateKey = xprivateKey
-    val publicKey = xpublicKey
-
-    def getPrivateKey() : Array[Byte] ={
-       privateKey.getBytes()
-    }
-
-    def getPublicKey() : Array[Byte] ={
-       publicKey.getBytes()
     }
   }
   class KspB64 {
