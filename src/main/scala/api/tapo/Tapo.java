a9e7eeb92b0dece9dcf61bc446f989192a0fc906
swalsh <stephen.walsh@alvaria.com>
2021-12-02 12:14:45 +0000

    error handling

--- src/main/scala/api/tapo/Tapo.java
+++ src/main/scala/api/tapo/Tapo.java
@@ -21,79 +21,59 @@ public class Tapo {
     public Map<String, HandshakeResponse> handshakeResponse = new HashMap<>();
 
 
-    Config config = ConfigFactory.load();
-    String email = config.getString("tapo.username");
-    String password = config.getString("tapo.password");
-    List<String> addresses = Arrays.asList(config.getString("tapo.addresses").split(","));
+    public void Setup(String username, String password, String ipAddress) throws Exception {
 
+        Security.addProvider(new BouncyCastleProvider());
+        KspDebug.out("Generating keypair...");
+        TapoFlow tapoFlow = new TapoFlow(ipAddress);
 
-    public void Setup() throws Exception {
+        KspKeyPair kspKeyPair = KspEncryption.generateKeyPair();
 
-        for (Integer i =0; i < addresses.size();i++) {
-
-            Security.addProvider(new BouncyCastleProvider());
-            KspDebug.out("Generating keypair...");
-            TapoFlow tapoFlow = new TapoFlow(addresses.get(i));
-
-            KspKeyPair kspKeyPair = KspEncryption.generateKeyPair();
-
-            KspDebug.out("Sending handshake!");
-            if(handshakeResponse.containsKey(addresses.get(i)))
-            {
-                handshakeResponse.computeIfPresent(addresses.get(i), (k, v) -> tapoFlow.makeHandshake(kspKeyPair));
-                KspDebug.out("Updated handshake");
-            }
-            else
-            {
-                handshakeResponse.put(addresses.get(i),tapoFlow.makeHandshake(kspKeyPair));
-                KspDebug.out("Created handshake");
-            }
-
-            String keyFromTapo = handshakeResponse.get(addresses.get(i)).getResponse().getAsJsonObject("result").get("key").getAsString();
-            KspDebug.out("Tapo's key is: " + keyFromTapo);
-            KspDebug.out("Our session cookie is: " + handshakeResponse.get(addresses.get(i)).getCookie());
-
-            KspDebug.out("Will try to decode it!");
-            if(c658a.containsKey(addresses.get(i)))
-            {
-                c658a.computeIfPresent(addresses.get(i), (k, v) -> KspEncryption.decodeTapoKey(keyFromTapo, kspKeyPair));
-                KspDebug.out("Updated c658a");
-            }
-            else
-            {
-                c658a.put(addresses.get(i),KspEncryption.decodeTapoKey(keyFromTapo, kspKeyPair));
-                KspDebug.out("Created c658a");
-            }
-
-            KspDebug.out("Decoded!");
-
-            KspDebug.out("Will try to login!");
-            JsonObject resp = tapoFlow.loginRequest(email, password, c658a.get(addresses.get(i)), handshakeResponse.get(addresses.get(i)).getCookie());
-
-            if(token.containsKey(addresses.get(i)))
-            {
-                token.computeIfPresent(addresses.get(i), (k, v) -> resp.getAsJsonObject("result").get("token").getAsString());
-                KspDebug.out("Updated token");
-            }
-            else
-            {
-                token.put(addresses.get(i),resp.getAsJsonObject("result").get("token").getAsString());
-                KspDebug.out("Created token");
-            }
-            KspDebug.out("Got token: " + token.get(addresses.get(i)));
-            tapoFlow.getPlugEnergyUsage(c658a.get(addresses.get(i)), token.get(addresses.get(i)), handshakeResponse.get(addresses.get(i)).getCookie());
+        KspDebug.out("Sending handshake!");
+        if (handshakeResponse.containsKey(ipAddress)) {
+            handshakeResponse.computeIfPresent(ipAddress, (k, v) -> tapoFlow.makeHandshake(kspKeyPair));
+            KspDebug.out("Updated handshake");
+        } else {
+            handshakeResponse.put(ipAddress, tapoFlow.makeHandshake(kspKeyPair));
+            KspDebug.out("Created handshake");
         }
+
+        String keyFromTapo = handshakeResponse.get(ipAddress).getResponse().getAsJsonObject("result").get("key").getAsString();
+        KspDebug.out("Tapo's key is: " + keyFromTapo);
+        KspDebug.out("Our session cookie is: " + handshakeResponse.get(ipAddress).getCookie());
+
+        KspDebug.out("Will try to decode it!");
+        if (c658a.containsKey(ipAddress)) {
+            c658a.computeIfPresent(ipAddress, (k, v) -> KspEncryption.decodeTapoKey(keyFromTapo, kspKeyPair));
+            KspDebug.out("Updated c658a");
+        } else {
+            c658a.put(ipAddress, KspEncryption.decodeTapoKey(keyFromTapo, kspKeyPair));
+            KspDebug.out("Created c658a");
+        }
+
+        KspDebug.out("Decoded!");
+
+        KspDebug.out("Will try to login!");
+        JsonObject resp = tapoFlow.loginRequest(username, password, c658a.get(ipAddress), handshakeResponse.get(ipAddress).getCookie());
+
+        if (token.containsKey(ipAddress)) {
+            token.computeIfPresent(ipAddress, (k, v) -> resp.getAsJsonObject("result").get("token").getAsString());
+            KspDebug.out("Updated token");
+        } else {
+            token.put(ipAddress, resp.getAsJsonObject("result").get("token").getAsString());
+            KspDebug.out("Created token");
+        }
+        System.out.println("Got new token for : "+ipAddress);
+        KspDebug.out("Got token: " + token.get(ipAddress));
+        tapoFlow.getPlugEnergyUsage(c658a.get(ipAddress), token.get(ipAddress), handshakeResponse.get(ipAddress).getCookie());
+
     }
 
-    public Map<String, Integer> Run() {
-        Map<String, Integer> reply = new HashMap<>();
-        for (Integer i = 0; i < addresses.size(); i++) {
-            TapoFlow tapoFlow = new TapoFlow(addresses.get(i));
-            KspDebug.out("Will try to Get Energy Info!");
-            Integer value = tapoFlow.getPlugEnergyUsage(c658a.get(addresses.get(i)), token.get(addresses.get(i)), handshakeResponse.get(addresses.get(i)).getCookie());
-            System.out.println("Tapo Energy: "+addresses.get(i)+" = "+value);
-            reply.put(addresses.get(i),value);
-       }
-        return reply;
+    public Integer Run(String ipAddress) {
+        TapoFlow tapoFlow = new TapoFlow(ipAddress);
+        KspDebug.out("Will try to Get Energy Info!");
+        Integer value = tapoFlow.getPlugEnergyUsage(c658a.get(ipAddress), token.get(ipAddress), handshakeResponse.get(ipAddress).getCookie());
+        System.out.println("Tapo Energy: " + ipAddress + " = " + value);
+        return value;
     }
 }
--- src/main/scala/api/tapo/tapoMiddleMan.scala
+++ src/main/scala/api/tapo/tapoMiddleMan.scala
@@ -1,9 +1,12 @@
 package api.tapo
 
+import com.typesafe.config.{Config, ConfigFactory}
 import metrics.KamonMetrics
 
 import java.util
+import java.util.{Arrays, HashMap, List, Map}
 import scala.collection.JavaConversions.mapAsScalaMap
+import scala.reflect.internal.util.Statistics.canEnable.||
 
 
 class tapoMiddleMan(tapoParamenter: Tapo) {
@@ -15,28 +18,40 @@ class tapoMiddleMan(tapoParamenter: Tapo) {
 
   def Run(): Unit = {
 
-    if (tapo.token.isEmpty) {
-      tapo.Setup()
+    val config = ConfigFactory.load
+    val username = config.getString("tapo.username")
+    val password = config.getString("tapo.password")
+    val addresses: Array[String] = config.getString("tapo.addresses").split(",")
+
+    var token = new util.HashMap[String, String]
+    addresses foreach { case (address) =>
+
+      if (tapo.token.getOrElse(address, "") == "") {
+        tapo.Setup(username, password, address)
+      }
     }
 
+
+
     //TODO Error handling
-    try {
-      val metrics: util.Map[String, Integer] = tapo.Run()
-      metrics foreach { case (key, value) => {
-        reporterKamon.tapoEnergyUsageCounter.increment((value / 100).toLong, "ipAddress", key)
-        if(value == 0)
-        {
-          reporterKamon.tapoEnergyUsageGauge.set(0, "ipAddress", key)
+
+    addresses foreach { case (address) => {
+      try {
+        val energyUsage = tapo.Run(address)
+        reporterKamon.tapoEnergyUsageCounter.increment((energyUsage / 100).toLong, "ipAddress", address)
+        if (energyUsage == 0) {
+          reporterKamon.tapoEnergyUsageGauge.set(0, "ipAddress", address)
         }
         else {
-          reporterKamon.tapoEnergyUsageGauge.set((value / 100).toLong, "ipAddress", key)
+          reporterKamon.tapoEnergyUsageGauge.set((energyUsage / 100).toLong, "ipAddress", address)
         }
       }
+      catch {
+        case ex: Exception =>
+          println("ERROR Tapo : "+address+" - clearing its token : " + ex.toString)
+          tapo.token.put(address,"")
       }
     }
-    catch {
-      case ex: Exception => println("ERROR Running Tapo - Refreshing tokens : " + ex.toString);
-        tapo.Setup()
     }
   }
 }
