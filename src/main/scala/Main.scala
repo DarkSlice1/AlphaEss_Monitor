e0c674a5480b2700204735f73b426195cdf49730
swalsh <stephen.walsh@alvaria.com>
2021-12-10 14:35:58 +0000

    system & host metrics

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -120,21 +120,7 @@ object Main extends App {
 
 
   private def startKamon(config: Config) = {
-    // val system = config.getBoolean("kamon.system-metrics.jvm.enabled")
-    // val host = config.getBoolean("kamon.system-metrics.host.enabled")
     println("Starting Kamon reporters...." + config.getStringList("kamon.reporters").toString)
     Kamon.init(config)
-    Try(Kamon.reconfigure(config)) match {
-      case Success(_) =>
-
-      //TODO come back to this - why did it break coming from kamon 1.0 to 2.0 ???
-      //        if (system || host) {
-      //          SystemMetrics.startCollecting()
-      //          println(s"Kamon system metrics are enabled[$system], ${if (system) "starting" else "not starting"}.")
-      //          println(s"Kamon host metrics are enabled[$host], ${if (host) "starting" else "not starting"}.")
-      //        }
-      case Failure(e) =>
-        println("failed", s"Failed to start Kamon with exception ${e.getLocalizedMessage}")
-    }
   }
 }
